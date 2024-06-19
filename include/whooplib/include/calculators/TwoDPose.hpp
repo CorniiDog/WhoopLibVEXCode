fb231c5bb7e6749710714014a8db02d8059b8d3a
ConnorAtmos <connor.sw.personal@gmail.com>
2024-06-19 01:48:27 -0500

    Additional comments

--- include/whooplib/include/calculators/TwoDPose.hpp
+++ include/whooplib/include/calculators/TwoDPose.hpp
@@ -8,9 +8,13 @@ public:
     TwoDPose(double x, double y, double yaw);
 
     // Overloaded * operator to combine two poses
+    // Think of it as Roblox's CF = CFrame1 * CFrame2
     TwoDPose operator*(const TwoDPose& other) const;
 
     // Method to compute the relative pose
+    // Think of it as Roblox's CFrame1:ToObjectSpace(CFrame2)
+    // in which it returns the pose of CFrame2 relative to CFrame1
+    // But in this case, TwoDPose other relative to TwoDPose self
     TwoDPose toObjectSpace(const TwoDPose& other) const;
 };
 
