c95cbb2f5cf149ee1c864ae45ec8f65bc400cf2b
ConnorAtmos <connor.sw.personal@gmail.com>
2024-06-21 18:06:53 -0500

    Added additional comments to wheel odometry

--- include/whooplib/include/calculators/WheelOdom.hpp
+++ include/whooplib/include/calculators/WheelOdom.hpp
@@ -1,27 +1,70 @@
 /**
  * General-use odometry class with X_position, Y_position, and
- * orientation_deg being the relevant outputs. This works for one
- * and two-tracker systems, and needs a gyro to get input angle.
- * Note: Gyro is counter-clockwise-positive when sent. This is to standardize rotation
+ * orientation_rad being the relevant outputs. This works for both one
+ * and two-tracker systems, and requires a gyro to determine the input angle.
+ * Note: The gyro input is counter-clockwise-positive and provided in radians,
+ * which standardizes the rotation. This odometry system is a modified version
+ * adapted from the Jar-Template, specifically tailored for academic use.
+ * Source: https://github.com/JacksonAreaRobotics/JAR-Template/blob/main/src/JAR-Template/odom.cpp
  */
 
 #ifndef ODOM_HPP
 #define ODOM_HPP
 
-
+/**
+ * Wheel Odometry Object
+ */
 class WheelOdom
 {
 private:
-  float ForwardTracker_center_distance;
-  float SidewaysTracker_center_distance;
-  float ForwardTracker_position;
-  float SideWaysTracker_position;
+  float ForwardTracker_center_distance; // Horizontal distance from the robot's center to the forward wheel's sensor.
+  float SidewaysTracker_center_distance; // Vertical distance from the robot's center to the sideways wheel's sensor.
+  float ForwardTracker_position; // Current horizontal position of the forward tracker in meters.
+  float SideWaysTracker_position; // Current vertical position of the sideways tracker in meters.
 public:
-  float X_position;
-  float Y_position;
-  float orientation_deg;
-  void set_position(float X_position, float Y_position, float orientation_deg, float ForwardTracker_position, float SidewaysTracker_position);
-  void update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_deg);
+  float X_position; // Field-centric X position of the robot in meters.
+  float Y_position; // Field-centric Y position of the robot in meters.
+  float orientation_rad; // Robot's orientation in radians, where 0 radians aligns with the positive Y-direction.
+
+  /**
+   * Setter method for tracker center distances.
+   * The forward tracker center distance is the horizontal distance from the 
+   * center of the robot to the center of the wheel the sensor is measuring.
+   * The sideways tracker center distance is the vertical distance from the 
+   * center of the robot to the center of the sideways wheel being measured.
+   * If there's really no sideways wheel we set the center distance to 0 and
+   * pretend the wheel never spins, which is equivalent to a no-drift robot.
+   * 
+   * @param ForwardTracker_center_distance A horizontal distance to the wheel center in meters.
+   * @param SidewaysTracker_center_distance A vertical distance to the wheel center in meters.
+   */
+  void set_position(float X_position, float Y_position, float orientation_rad, float ForwardTracker_position, float SidewaysTracker_position);
+
+  /**
+   * Resets the position, including tracking wheels.
+   * Position is field-centric, and orientation is such that 0 radians
+   * is in the positive Y direction. Orientation can be provided with 
+   * some flexibility, including less than 0 and greater than 2*pi.
+   * 
+   * @param X_position Field-centric x position of the robot.
+   * @param Y_position Field-centric y position of the robot.
+   * @param orientation_rad Field-centered, counter-clockwise-positive, orientation in radians.
+   * @param ForwardTracker_position Current position of the sensor in meters.
+   * @param SidewaysTracker_position Current position of the sensor in meters.
+   */
+  void update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_rad);
+
+  /**
+   * Does the odometry math to update position
+   * Uses the Pilons arc method outlined here: https://wiki.purduesigbots.com/software/odometry
+   * All the deltas are done by getting member variables and comparing them to 
+   * the input. Ultimately this all works to update the public member variable
+   * X_position. This function needs to be run at 200Hz or so for best results.
+   * 
+   * @param ForwardTracker_position Current position of the sensor in meters.
+   * @param SidewaysTracker_position Current position of the sensor in meters.
+   * @param orientation_rad Field-centered, counter-clockwise-positive, orientation in radians.
+   */
   void set_physical_distances(float ForwardTracker_center_distance, float SidewaysTracker_center_distance);
 };
 
--- include/whooplib/src/calculators/WheelOdom.cpp
+++ include/whooplib/src/calculators/WheelOdom.cpp
@@ -1,5 +1,4 @@
 #include "vex.h"
-
 #include "whooplib/include/calculators/WheelOdom.hpp"
 #include "whooplib/include/toolbox.hpp"
 
@@ -12,10 +11,9 @@
  * If there's really no sideways wheel we set the center distance to 0 and
  * pretend the wheel never spins, which is equivalent to a no-drift robot.
  * 
- * @param ForwardTracker_center_distance A horizontal distance to the wheel center in inches.
- * @param SidewaysTracker_center_distance A vertical distance to the wheel center in inches.
+ * @param ForwardTracker_center_distance A horizontal distance to the wheel center in meters.
+ * @param SidewaysTracker_center_distance A vertical distance to the wheel center in meters.
  */
-
 void WheelOdom::set_physical_distances(float ForwardTracker_center_distance, float SidewaysTracker_center_distance){
   this->ForwardTracker_center_distance = ForwardTracker_center_distance;
   this->SidewaysTracker_center_distance = SidewaysTracker_center_distance;
@@ -23,47 +21,42 @@ void WheelOdom::set_physical_distances(float ForwardTracker_center_distance, flo
 
 /**
  * Resets the position, including tracking wheels.
- * Position is field-centric, and orientation is such that 0 degrees
+ * Position is field-centric, and orientation is such that 0 radians
  * is in the positive Y direction. Orientation can be provided with 
- * some flexibility, including less than 0 and greater than 360.
+ * some flexibility, including less than 0 and greater than 2*pi.
  * 
  * @param X_position Field-centric x position of the robot.
  * @param Y_position Field-centric y position of the robot.
- * @param orientation_deg Field-centered, counter-clockwise-positive, orientation.
- * @param ForwardTracker_position Current position of the sensor in inches.
- * @param SidewaysTracker_position Current position of the sensor in inches.
+ * @param orientation_rad Field-centered, counter-clockwise-positive, orientation in radians.
+ * @param ForwardTracker_position Current position of the sensor in meters.
+ * @param SidewaysTracker_position Current position of the sensor in meters.
  */
-
-void WheelOdom::set_position(float X_position, float Y_position, float orientation_deg, float ForwardTracker_position, float SidewaysTracker_position){
+void WheelOdom::set_position(float X_position, float Y_position, float orientation_rad, float ForwardTracker_position, float SidewaysTracker_position){
   this->ForwardTracker_position = ForwardTracker_position;
   this->SideWaysTracker_position = SidewaysTracker_position;
   this->X_position = X_position;
   this->Y_position = Y_position;
-  this->orientation_deg = orientation_deg;
+  this->orientation_rad = orientation_rad;
 }
 
 /**
  * Does the odometry math to update position
- * Uses the Pilons arc method outline here: https://wiki.purduesigbots.com/software/odometry
+ * Uses the Pilons arc method outlined here: https://wiki.purduesigbots.com/software/odometry
  * All the deltas are done by getting member variables and comparing them to 
  * the input. Ultimately this all works to update the public member variable
  * X_position. This function needs to be run at 200Hz or so for best results.
  * 
- * @param ForwardTracker_position Current position of the sensor in inches.
- * @param SidewaysTracker_position Current position of the sensor in inches.
- * @param orientation_deg Field-centered, counter-clockwise-positive, orientation.
+ * @param ForwardTracker_position Current position of the sensor in meters.
+ * @param SidewaysTracker_position Current position of the sensor in meters.
+ * @param orientation_rad Field-centered, counter-clockwise-positive, orientation in radians.
  */
-
-void WheelOdom::update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_deg){
-  // this-> always refers to the old version of the variable, so subtracting this->x from x gives delta x.
+void WheelOdom::update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_rad){
   float Forward_delta = ForwardTracker_position - this->ForwardTracker_position;
   float Sideways_delta = SidewaysTracker_position - this->SideWaysTracker_position;
   this->ForwardTracker_position = ForwardTracker_position;
   this->SideWaysTracker_position = SidewaysTracker_position;
-  float orientation_rad = to_rad(orientation_deg);
-  float prev_orientation_rad = to_rad(this->orientation_deg);
-  float orientation_delta_rad = orientation_rad - prev_orientation_rad;
-  this->orientation_deg = orientation_deg;
+  float orientation_delta_rad = orientation_rad - this->orientation_rad;
+  this->orientation_rad = orientation_rad;
 
   float local_X_position;
   float local_Y_position;
@@ -76,18 +69,10 @@ void WheelOdom::update_position(float ForwardTracker_position, float SidewaysTra
     local_Y_position = (2 * sin(orientation_delta_rad / 2)) * ((Forward_delta / orientation_delta_rad) + ForwardTracker_center_distance);
   }
 
-  float local_polar_angle;
-  float local_polar_length;
+  float local_polar_angle = atan2(local_Y_position, local_X_position); 
+  float local_polar_length = sqrt(pow(local_X_position, 2) + pow(local_Y_position, 2)); 
 
-  if (local_X_position == 0 && local_Y_position == 0) {
-    local_polar_angle = 0;
-    local_polar_length = 0;
-  } else {
-    local_polar_angle = atan2(local_Y_position, local_X_position); 
-    local_polar_length = sqrt(pow(local_X_position, 2) + pow(local_Y_position, 2)); 
-  }
-
-  float global_polar_angle = local_polar_angle + prev_orientation_rad + (orientation_delta_rad / 2);
+  float global_polar_angle = local_polar_angle + this->orientation_rad + (orientation_delta_rad / 2);
 
   float X_position_delta = local_polar_length * cos(global_polar_angle); 
   float Y_position_delta = local_polar_length * sin(global_polar_angle);
