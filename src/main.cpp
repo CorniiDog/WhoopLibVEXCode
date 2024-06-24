177aacb3a092c6ec395033666e110b11134feaa7
ConnorAtmos <connor.sw.personal@gmail.com>
2024-06-23 19:17:21 -0500

    Neatified some of the code

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,13 @@ using namespace vex;
 competition Competition;
 
 // Serial communication module
-BufferNode buffer_system(256, debugMode::debug_disabled, "/dev/serial1"); // set to debug_disabled for competition
+BufferNode buffer_system(
+  256, // The buffer size, in characters. Increase if necessary, but at the cost of computational efficiency.
+  debugMode::debug_disabled, // debugMode::debug_disabled for competition use, debugMode::debug_enabled to allow the code to pass errors through
+  "/dev/serial1" // The serial connection of the Jetson Nano ("/dev/serial1" is the micro-usb serial connection on the V5 Brain)
+); 
 
 WhoopController controller1(joystickMode::joystickmode_split_arcade);
-
-// Right drive motors
-WhoopMotor r1(PORT1, gearSetting::ratio6_1, reversed::yes_reverse);
-WhoopMotor r2(PORT2, gearSetting::ratio6_1, reversed::yes_reverse);
-WhoopMotor r3(PORT3, gearSetting::ratio6_1, reversed::yes_reverse);
-WhoopMotor r4(PORT4, gearSetting::ratio6_1, reversed::yes_reverse);
-WhoopMotorGroup right_motors({&r1, &r2, &r3, &r4});
 
 // Left drive motors
 WhoopMotor l1(PORT12, gearSetting::ratio6_1, reversed::no_reverse);
@@ -38,51 +35,58 @@ WhoopMotor l3(PORT14, gearSetting::ratio6_1, reversed::no_reverse);
 WhoopMotor l4(PORT15, gearSetting::ratio6_1, reversed::no_reverse);
 WhoopMotorGroup left_motors({&l1, &l2, &l3, &l4});
 
-// Vision Offset from Center of Robot
-// First variable is x, which +x is the direction of right from the center of the robot in meters
-// Second variable is y, which +y is the direction of forwardness from the center of the robot in meters
-double vision_x_offset_m = 0;
-double vision_y_offset_m = 15.0/100.0;
-RobotVisionOffset vision_offset(vision_x_offset_m, vision_y_offset_m);
+// Right drive motors
+WhoopMotor r1(PORT1, gearSetting::ratio6_1, reversed::yes_reverse);
+WhoopMotor r2(PORT2, gearSetting::ratio6_1, reversed::yes_reverse);
+WhoopMotor r3(PORT3, gearSetting::ratio6_1, reversed::yes_reverse);
+WhoopMotor r4(PORT4, gearSetting::ratio6_1, reversed::yes_reverse);
+WhoopMotorGroup right_motors({&r1, &r2, &r3, &r4});
 
-// Jetson Nano pose retreival stream identifier (configured on Nano-side) 
-std::string pose_stream = "P";
-WhoopVision vision_system(&vision_offset, &buffer_system, pose_stream);
+// Vision Offset of the Vision Tesseract from the Center of Robot
+RobotVisionOffset vision_offset(
+  0.00, // The x offset (+x is the direction of right from the center of the robot in meters).
+  15.0/100.0 // The y offset (+y is the direction of forwardness from the center of the robot in meters).
+);
 
-//Gear ratio on the drivetrain (If it's a [motor powering the 32t] driving the [64t w/ shared axle to the wheels], it would be a 1.0/2.0 gear ratio, or 0.5)
-double gear_ratio = 1.0/2.0;
-
-// Wheel diameter in meters
-double wheel_diameter_m = to_meters(3); 
+// Jetson Nano pose retreival object (also configured on Nano-side) 
+WhoopVision vision_system(
+  &vision_offset, // pointer to the vision offset
+  &buffer_system, // Pointer to the buffer system (will be managed by the buffer system)
+  "P" // The string representing the subscribed stream to receive the pose from the Jetson Nano
+);
 
 WhoopInertial inertial_sensor(PORT7);
 WhoopRotation forward_tracker(PORT6, reversed::yes_reverse);
 WhoopRotation sideways_tracker(PORT9, reversed::no_reverse);
 
-double forward_wheel_diameter_m = to_meters(2.5189); // (e.g., 0.08255 for 3.25-inch wheels).
-double sideways_wheel_diameter_m = to_meters(2.5189);
-
-/**
- * SUGGESTION: It is easier and likely more precise to measure the distances by placing the robot over a glass table to allow the odometry wheels to recess to default 
- * position and measure from below.
- */
-
 // The odom unit center is the virtual intercept of the perpendicular faces of the odometry trackers.
 // The measurement is from the center of the odom unit to the designated tracker distances.
 // Visual Representation of Tracker Distances from Odom Unit: https://imgur.com/rWCCCfz
-double forward_tracker_distance_m = to_meters(1.51); // Distance from the odom unit center to the forward tracker, in meters (positive implies a shift to the right from the odom unit center).
-double sideways_tracker_distance_m = to_meters(-4.468); // Distance from the odom unit center to the sideways tracker, in meters (positive implies a shift forward from the odom unit center).
-WhoopDriveOdomUnit odom_unit(forward_tracker_distance_m, forward_wheel_diameter_m, sideways_tracker_distance_m, sideways_wheel_diameter_m, &inertial_sensor, &forward_tracker, &sideways_tracker);
+WhoopDriveOdomUnit odom_unit(
+  to_meters(1.51), // The forward tracker distance, in meters, from the odom unit's center. (positive implies a shift to the right from the odom unit's center)
+  to_meters(2.5189), // Diameter of the forward tracker, in meters (e.g., 0.08255 for 3.25-inch wheels).
+  to_meters(-4.468), // The sideways tracker distance, in meters, from the odom unit's center (positive implies a shift forward from the odom unit center).
+  to_meters(2.5189), // Diameter of the sideways tracker, in meters (e.g., 0.08255 for 3.25-inch wheels).
+  &inertial_sensor, // Pointer to the WhoopInertial sensor
+  &forward_tracker, // Pointer to the forward tracker, as a WhoopRotation sensor
+  &sideways_tracker // Pointer to the sideways tracker, as a WhoopRotation sensor
+);
 
 // If your Odom Unit's Center is NOT the center of the robot, apply the offset here.
 // The measurement is from the center of the robot to the odom unit center.
 // If your Odom Unit's Center IS the center of the robot, set to 0,0.
 // Visual representation of Odom Unit from Center of Robot: https://imgur.com/x8ObCIG
-double odom_x_offset_m = to_meters(-0.6); // The x offset of the odom unit from the center of the robot (positive implies a shift right from the center of the robot).
-double odom_y_offset_m = to_meters(4.95); // The y offset of the odom unit from the center of the robot (positive implies a shift forward from the center of the robot).
-WhoopDriveOdomOffset odom_offset(&odom_unit, odom_x_offset_m, odom_y_offset_m);
+WhoopDriveOdomOffset odom_offset(
+  &odom_unit, // Pointer to the odometry unit (will manage the odom unit)
+  to_meters(-0.6), // The x offset of the odom unit from the center of the robot (positive implies a shift right from the center of the robot).
+  to_meters(4.95) // The y offset of the odom unit from the center of the robot (positive implies a shift forward from the center of the robot).
+);
 
-WhoopDrivetrain robot_drivetrain(&controller1, &left_motors, &right_motors);
+WhoopDrivetrain robot_drivetrain(
+  &controller1, // Pointer to the controller that controls the drivetrain
+  &left_motors, // Pointer to the left motor group (optionally can be a list of motors as well)
+  &right_motors // Pointer to the right motor group (optionally can be a list of motors as well)
+);
 
 ComputeManager manager({&buffer_system, &robot_drivetrain, &odom_offset});
 
