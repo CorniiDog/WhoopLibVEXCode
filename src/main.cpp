e435dba0f5be8db347c981f4a11750e13119ccc6
ConnorAtmos <connor.sw.personal@gmail.com>
2024-06-27 19:38:47 -0500

    Updated some variables

--- src/main.cpp
+++ src/main.cpp
@@ -182,9 +182,9 @@ WhoopOdomFusion odom_fusion(
   &odom_offset, // Pointer to the odometry offset
   0.5, // Minimum confidence threshold to apply vision system to odometry
   FusionMode::fusion_gradual, // The method of fusing
-  to_meters(2), // If FusionMode is fusion_gradual, it is the maximum allowable shift in meters for gradual fusion, per second.
-  to_rad(10), // If FusionMode is fusion_gradual, it is the maximum allowable rotational shift of the yaw in radians for gradual fusion, per second.
-  1 // Feedforward gain of the vision system as it has delay, in milliseconds (For 100Hz Wheel Odometry)
+  to_meters(10), // If FusionMode is fusion_gradual, it is the maximum allowable shift in meters for gradual fusion, per second.
+  to_rad(45), // If FusionMode is fusion_gradual, it is the maximum allowable rotational shift of the yaw in radians for gradual fusion, per second.
+  5 // Feedforward gain of the vision system as it has delay, in milliseconds (For 100Hz Wheel Odometry)
 );
 
 ////////////////////////////////////////////////////////////
@@ -217,9 +217,10 @@ void pre_auton(void) {
   vexcodeInit();
   manager.start();
   robot_drivetrain.set_state(drivetrainState::mode_disabled);
-  controller1.notify("Initializing...");
-  wait(0.5, sec); // The serial port may take some time 
+  controller1.notify("Initializing Robot");
   jetson_commander.initialize();
+  robot_drivetrain.calibrate();
+  controller1.notify("Calibration Complete");
 }
 
 /*---------------------------------------------------------------------------*/
